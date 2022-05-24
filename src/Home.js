1f5db1383e8c3b2a0ebfc743a5104d8d333dc90c
Luanftg <luanftgimenez@gmail.com>
2022-05-24 16:31:59 -0300

    add JSON.response to LocalStorage

--- src/Home.js
+++ src/Home.js
@@ -4,13 +4,21 @@ import axios from 'axios';
 function App(props) {
   const [usuario,setUsuario] = useState('');  
   function handlePesquisa() {
-    axios.get(`https://api.github.com/users/${usuario}/repos`).then(response => console.log(response.data));
+    axios.get(`https://api.github.com/users/${usuario}/repos`).then(response => {
+      const repositories = response.data;
+      const repositoriesName = [];
+      repositories.map((repository) => {
+        repositoriesName.push(repository.name);
+      });
+      localStorage.setItem('repositoriesName', JSON.stringify(repositoriesName))
+
+    });
   }
     return (
 
       <>
         <p>{usuario}</p>
-        <input className="usuario" placeholder="usuário" value={usuario} onChange={e => setUsuario(e.target.value)}/>
+        <input className="usuario" placeholder="Usuário" value={usuario} onChange={e => setUsuario(e.target.value)}/>
         <button type="button" onClick={handlePesquisa}>Pesquisar</button>
       </>
     );
